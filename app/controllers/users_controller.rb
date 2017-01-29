5b2b5afd5ce3c516d696ae2c58b80ac33f57f0a8
Mark Miranda <notmarkmiranda@gmail.com>
2017-01-28 20:03:41 -0700

    add testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome!"
       redirect_to dashboard_path
     else
+      # binding.pry
       flash.now[:warning] = "Something went wrong!"
       render :new
     end
